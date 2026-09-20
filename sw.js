e32bfdba7e620cf9ec87b2513b532c5f70674894
VIC <vic@VICs-iMac.local>
2026-09-20 02:01:35 -0700

    styleChange

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v21.76';
+const CACHE_NAME = 'elden-EARTH-v21.77';
 
 const ASSETS_TO_CACHE = [
     './',
