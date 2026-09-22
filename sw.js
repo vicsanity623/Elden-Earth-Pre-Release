160c3c5fb385e36b593e0da582ea4f6e2d8ae66d
VIC <vic@VICs-iMac.local>
2026-09-22 01:16:53 -0700

    petPlotPatch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v22.36';
+const CACHE_NAME = 'elden-EARTH-v22.37';
 
 const ASSETS_TO_CACHE = [
     './',
