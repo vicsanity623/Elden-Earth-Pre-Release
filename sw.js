c3acaaf853b02b911c3ad93512f603d9a87f0ae4
VIC <vic@VICs-iMac.local>
2026-09-24 02:52:42 -0700

    stylebuttons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v22.62';
+const CACHE_NAME = 'elden-EARTH-v22.63';
 
 const ASSETS_TO_CACHE = [
     './',
