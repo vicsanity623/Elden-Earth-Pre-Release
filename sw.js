2f109097c7623807a30c61deb6540a54dd7abc54
VIC <vic@VICs-iMac.local>
2026-09-24 21:33:56 -0700

    RemoveLP

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v22.75';
+const CACHE_NAME = 'elden-EARTH-v22.77';
 
 const ASSETS_TO_CACHE = [
     './',
